a88d87514d4a5bf3774f044063c79ebe560c1927
Kirari2 <k2233@duck.com>
2023-01-18 23:12:11 +0800

    Updates

--- plot6.R
+++ plot6.R
@@ -3,7 +3,7 @@
 # from motor vehicle sources in Los Angeles County, California (fips == "06037"),
 # Which city has seen greater changes over time in motor vehicle emissions?
 
-png(file = "./plot6.png", width = 8, height = 5)
+png(file = "./plot6.png")
 NEI %>% 
     filter(type == "ON-ROAD" & (fips == "24510" | fips == "06037")) %>%
     mutate(fips = recode(fips, "24510" = "Baltimore City", "06037" = "Los Angeles")) %>% 
@@ -13,7 +13,7 @@ NEI %>%
             facet_wrap(vars(fips)) +
             theme_bw() +
             theme(legend.position = "none") +
-            labs(title = "PM2.5 Emissions from Motor Vehicle Sources in Baltimore City and Los Angeles, 1999–2008", 
+            labs(title = "PM2.5 Emissions from Motor Vehicle Sources in Baltimore City \nand Los Angeles, 1999–2008", 
                  x = "Year",
                  y = "PM2.5 emissions (tons)")
 dev.off()
